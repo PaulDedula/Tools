a155ed24ef949690c55b42be6afb27e60c8de07c
PaulDedula <103753391+PaulDedula@users.noreply.github.com>
2022-05-01 20:17:06 -0400

    Removed var assignment

--- src/Management/Benchmark.ps1
+++ src/Management/Benchmark.ps1
@@ -1,36 +1,35 @@
 # Get-Childitem
 ## Include switch
-$GetChildItem = Measure-Command { $res = Get-Childitem -Path C:\users\pauld\ -Include "*.csv", "*.txt", "*.ps1" -Recurse }
+$GetChildItem = Measure-Command { Get-Childitem -Path C:\users\pauld\ -Include "*.csv", "*.txt", "*.ps1" -Recurse }
 clear-variable -Name res
 
 
 ## Loop through several extensions with Foreach-object
-$GetChildItem_Loop = Measure-Command { $res = "*.csv", "*.txt", "*.ps1" | ForEach-Object { Get-Childitem -Path C:\users\pauld\ -Filter $_ -Recurse } }
+$GetChildItem_Loop = Measure-Command { "*.csv", "*.txt", "*.ps1" | ForEach-Object { Get-Childitem -Path C:\users\pauld\ -Filter $_ -Recurse } }
 clear-variable -Name res
 
 
 ## Loop through several extensions with Foreach-object Parallel
-$GetChildItem_Loop_Parallel = Measure-Command { $res = "*.csv", "*.txt", "*.ps1" | ForEach-Object -Parallel { Get-Childitem -Path C:\users\pauld\ -Filter $_ -Recurse } }
+$GetChildItem_Loop_Parallel = Measure-Command { "*.csv", "*.txt", "*.ps1" | ForEach-Object -Parallel { Get-Childitem -Path C:\users\pauld\ -Filter $_ -Recurse } }
 clear-variable -Name res
 
 
 # Dir
 ## loop through several extensions with foreach-object
-$dir_loop = Measure-Command { $res = "*.csv", "*.txt", "*.ps1" | ForEach-Object { C:\windows\system32\cmd.exe /c dir /s /b "C:\users\pauld\$_" } }
+$dir_loop = Measure-Command { "*.csv", "*.txt", "*.ps1" | ForEach-Object { C:\windows\system32\cmd.exe /c dir /s /b "C:\users\pauld\$_" } }
 clear-variable -Name res
 
 
 ## loop through several extensions with foreach-Object parallel
-$dir_loop_Parallel = Measure-Command { $res = "*.csv", "*.txt", "*.ps1" | ForEach-Object -parallel { C:\windows\system32\cmd.exe /c dir /s /b "C:\users\pauld\$_" } }
+$dir_loop_Parallel = Measure-Command { "*.csv", "*.txt", "*.ps1" | ForEach-Object -parallel { C:\windows\system32\cmd.exe /c dir /s /b "C:\users\pauld\$_" } }
 clear-variable -Name res
 
 $dir_string = Measure-Command { Start-Process C:\windows\system32\cmd.exe -NoNewWindow -Wait -RedirectStandardOutput temp:\fileResults.txt -ArgumentList "/c dir /s /b C:\users\pauld\*.txt && dir /s /b C:\users\pauld\*.ps1 && dir /s /b C:\users\pauld\*.csv" }
 
 
 # Get-Files
-$GetFiles = measure-Command { $res = Get-Files -Path C:\users\pauld\ -Include "*.csv", "*.txt", "*.ps1" -Recurse }
-$GetFiles_Parallel = measure-Command { $res = Get-Files -Path C:\users\pauld\ -Include "*.csv", "*.txt", "*.ps1" -Recurse -Parallel }
-
+$GetFiles = measure-Command { Get-Files -Path C:\users\pauld\ -Include "*.csv", "*.txt", "*.ps1" -Recurse }
+$GetFiles_Parallel = measure-Command { Get-Files -Path C:\users\pauld\ -Include "*.csv", "*.txt", "*.ps1" -Recurse -Parallel }
 
 
 $output = {
