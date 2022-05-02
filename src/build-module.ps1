33c723d7bbefc43fa746417ad606c8bfd66a786f
PaulDedula <103753391+PaulDedula@users.noreply.github.com>
2022-05-01 20:35:25 -0400

    Update build script

--- src/build-module.ps1
+++ src/build-module.ps1
@@ -1,5 +1,5 @@
 Param (
-    [string]$docsFolder = "docs\en-us"
+    [string]$docsFolder = "en-US\"
 )
 
 $Modules = Get-ChildItem $PSScriptRoot -filter *.psm1 -Recurse
@@ -8,9 +8,14 @@ foreach ($m in $modules) {
     Import-module $m -Force
     $moduleRoot = Split-Path $m.FullName -Parent
     $docsPath = $(Join-Path -Path $moduleRoot -ChildPath $docsFolder)
+    if (-not (test-path $docsPath)) {
+        New-Item -ItemType directory -Path $docsPath
+        New-MarkdownHelp -Module $m.BaseName -OutputPath $docsPath
+        New-MarkdownAboutHelp -AboutName $m.BaseName -OutputFolder $docsPath
+    }
     # Run tests
     # Update MD docs
     # Build ExternalHelp
     Update-MarkdownHelp -Path $docsPath
-    New-ExternalHelp -Path $docsPath -OutputPath $moduleRoot -Force
+    New-ExternalHelp -Path $docsPath -OutputPath $docsPath -Force
 }
